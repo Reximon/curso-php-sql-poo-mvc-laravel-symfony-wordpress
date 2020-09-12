962c0b6b71473db38b3e0e6a56568a20db5f43f6
Reximon <reximon@gmail.com>
2020-09-12 23:03:46 +0200

    Añadadido un ejemplo para el Update

--- aprendiendo-sql/04-dml/update.sql
+++ aprendiendo-sql/04-dml/update.sql
@@ -1,3 +1,5 @@
 -- Modificar filas / Actualizar Datos --
 
 UPDATE usuarios SET fecha = '2021-07-09', apellidos = 'admin' WHERE id = 4;
+-- Ejemplos 
+UPDATE `entradas` SET `id` = '11' WHERE `entradas`.`id` = 13; 
