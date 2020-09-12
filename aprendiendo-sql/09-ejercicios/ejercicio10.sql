ede4ce53a585787a38d93d5d14f2bb3c9cbb718d
Reximon <reximon@gmail.com>
2020-09-12 14:20:26 +0200

    Ejercicios SQL

---
+++ aprendiendo-sql/09-ejercicios/ejercicio10.sql
@@ -0,0 +1,5 @@
+-- 10.- Visualizar los apellidos de los vendedores, su fecha y su número de grupo ordenado por fecha descendente, y mostrar los 4 últimos
+
+SELECT apellidos, fecha, id FROM vendedores ORDER BY fecha DESC LIMIT 4;
+
+-- A medias hecho por mi mismo :////
