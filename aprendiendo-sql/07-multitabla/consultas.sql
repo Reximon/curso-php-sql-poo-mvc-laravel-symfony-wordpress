d5efd0b67f8677a2374f33ae6cceac1d42a2bcc6
Reximon <reximon@gmail.com>
2020-09-12 23:04:40 +0200

    Consultas Multitablas

--- aprendiendo-sql/07-multitabla/consultas.sql
+++ aprendiendo-sql/07-multitabla/consultas.sql
@@ -7,3 +7,17 @@ Consultas multitablas:
 
 SELECT e.id, e.titulo, u.nombre AS 'Autor', c.nombre AS 'Categoria' FROM entradas e, usuarios u, categorias c WHERE e.usuario_id = u.id AND e.categoria_id = c.id; 
 SELECT e.id, e.titulo, u.nombre AS 'Autor', c.nombre AS 'Categoria' FROM entradas e, usuarios u, categorias c WHERE e.usuario_id = u.id; 
+
+-- Mostrar el nombre de las categorías y al lado cuantas entradas tienen --
+SELECT c.nombre, count(e.id) FROM categorias c, entradas e WHERE e.categoria_id = c.id GROUP BY e.categoria_id;
+
+SELECT c.nombre, count(e.id) FROM categorias c LEFT JOIN entradas e ON e.categoria_id = c.id GROUP BY e.categoria_id;
+
+SELECT c.nombre, count(e.id) FROM entradas e RIGHT JOIN categorias c ON e.categoria_id = c.id GROUP BY e.categoria_id;
+-- Mostrar el email de los usuarios y al lado cuantas entradas tienen --
+SELECT u.email, COUNT(titulo) FROM usuarios u, entradas e WHERE e.usuario_id = u.id GROUP BY e.usuario_id;
+
+-- Inner Join
+
+SELECT e.id, e.titulo, u.nombre AS 'Autor', c.nombre AS 'Categoria'  FROM
+entradas e INNER JOIN usuarios u ON e.usuario_id = u.id INNER JOIN categorias c ON e.categoria_id = c.id;
