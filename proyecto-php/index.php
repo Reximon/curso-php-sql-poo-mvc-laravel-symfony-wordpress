7bf936b37ade2de131a7be98faa2c883a79040ee
Reximon <reximon@gmail.com>
2020-10-09 15:35:14 +0200

    Cambios mínimos

--- proyecto-php/index.php
+++ proyecto-php/index.php
@@ -6,12 +6,12 @@
     <div id="principal">
     <h1>Últimas entradas</h1>
         <?php 
-            $entradas = conseguirUltimasEntradas($db);
+            $entradas = conseguirEntradas($db, true);
             if(!empty($entradas)):
                 while($entrada = mysqli_fetch_assoc($entradas)):
         ?>
                 <article class="entrada">
-                    <a href="">
+                    <a href="entrada.php?id=<?=$entrada['id']?>">
                         <h2><?=$entrada['titulo']?></h2>
                         <span class="date"><?= $entrada['categoria'].' | '.$entrada['fecha'] ?></span>
                             <p>
@@ -25,7 +25,7 @@
             endif;
         ?>
         <div id="ver-todas">
-            <a href="">Ver todas las entradas</a>
+            <a href="entradas.php">Ver todas las entradas</a>
         </div>
     </div> <!-- Fin principal-->
 <?php require_once 'includes/pie.php' ?>
